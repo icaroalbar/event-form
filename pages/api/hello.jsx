a5ed77017333da5884e088e652773fceabed9d4e
icaro_albar <icaro.albar@live.com>
2022-08-10 14:28:04 -0300

    VA teste 02

--- pages/api/hello.jsx
+++ pages/api/hello.jsx
@@ -5,47 +5,45 @@ const nodemailer = require("nodemailer");
 export default function sendMail(req, res) {
 
     let vendedores = [
-        {nome: 'Agatha Medeiros', email: 'agatha.medeiros@hpcap.com.br'},
-        {nome: 'Alessandra Tavares', email: 'alessandra.tavares@hpcap.com.br'},
-        {nome: 'Claudio  Amoedo', email: 'claudio.amoedo@hpcap.com.br'},
-        {nome: 'Eduardo Torres', email: 'eduardo.torres@hpcap.com.br'},
-        {nome: 'Felipe Rodrigues', email: 'felipe.rodrigues@bankhp.com.br'},
-        {nome: 'Icaro Albar', email: 'icaro.albar@hpcap.com.br'},
-        {nome: 'Jocyele Batista', email: 'jocyele.batista@hpcap.com.br'},
-        {nome: 'Jose Lucas', email: 'jose.lucas@hpcap.com.br'},
-        {nome: 'Leandro Ribeiro', email: 'leandro.ribeiro@hpcap.com.br'},
-        {nome: 'Luciano Deccache', email: 'luciano.deccache@hpcap.com.br'},
-        {nome: 'Lucy Deccache', email: 'lucy.deccache@hpcap.com.br'},
-        {nome: 'Luiz Paulo', email: 'luiz.paulo@hpcap.com.br'},
-        {nome: 'Marco Alves', email: 'marco.correa@hpcap.com.br'},
-        {nome: 'Patrícia Cruz', email: 'patricia.cruz@bankhp.com.br'},
-        {nome: 'Outros', email: ''}
-        
-
-  ]
-
-let vendedor = vendedores[req.body.consultor]
-
-let vendedorNome = vendedor.nome
-let vendedorEmail = vendedor.email
+        { nome: 'Agatha Medeiros', email: 'agatha.medeiros@hpcap.com.br' },
+        { nome: 'Alessandra Tavares', email: 'alessandra.tavares@hpcap.com.br' },
+        { nome: 'Claudio  Amoedo', email: 'claudio.amoedo@hpcap.com.br' },
+        { nome: 'Eduardo Torres', email: 'eduardo.torres@hpcap.com.br' },
+        { nome: 'Icaro Albar', email: 'icaro.albar@hpcap.com.br' },
+        { nome: 'Jocyele Batista', email: 'jocyele.batista@hpcap.com.br' },
+        { nome: 'Jose Lucas', email: 'jose.lucas@hpcap.com.br' },
+        { nome: 'Leandro Ribeiro', email: 'leandro.ribeiro@hpcap.com.br' },
+        { nome: 'Luciano Deccache', email: 'luciano.deccache@hpcap.com.br' },
+        { nome: 'Lucy Deccache', email: 'lucy.deccache@hpcap.com.br' },
+        { nome: 'Luiz Paulo', email: 'luiz.paulo@hpcap.com.br' },
+        { nome: 'Marco Alves', email: 'marco.correa@hpcap.com.br' },
+        { nome: 'Patrícia Cruz', email: 'patricia.cruz@bankhp.com.br' },
+        { nome: 'Outros', email: '' }
 
 
-let transporter = nodemailer.createTransport({
-    host: 'email-smtp.us-east-1.amazonaws.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: 'AKIA6JQEFCPPAZGMEHF6',
-        pass: 'BEtrjzS6npEP3WhEu5yqtTekyRsLGC4JQmPCU3xspHWc',
-    }
-})
+    ]
 
-transporter.sendMail({
-    from: "Contato Grupo HP <icaro.albar@gmail.com>",
-    to: `icaro.albar@hpcap.com.br, ${vendedorEmail}`,
-    subject: "Mensagem do site",
-    text: `${req.body.nome}`,
-    html: `<style>
+    let vendedor = vendedores[req.body.consultor]
+
+    let vendedorNome = vendedor.nome
+    let vendedorEmail = vendedor.email
+
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: "222bae44635930",
+            pass: "ab001434742ceb"
+        }
+    });
+
+    transporter.sendMail({
+        from: "Contato Grupo HP <icaro.albar@gmail.com>",
+        to: `icaro.albar@hpcap.com.br, ${vendedorEmail}`,
+        subject: "Mensagem do site",
+        text: `${req.body.nome}`,
+        html: `<style>
     a {
         text-decoration: none;
     }
@@ -111,6 +109,6 @@ transporter.sendMail({
         <a href="https://hpcap.com.br/">Grupo HP</a>
         <footer>
 </div>`
-}).then(response => { res.send(response) })
-    .catch(erro => { res.send(erro) })
+    }).then(response => { res.send(response) })
+        .catch(erro => { res.send(erro) })
 }
